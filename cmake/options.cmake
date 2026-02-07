0bdf86607db3d90aaab72e5e5036ccffe5747c48
ZephyrousChimes <adam.footsteps@gmail.com>
2026-02-07 18:50:47 +0530

    fix: added native arch compilation option

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -2,3 +2,5 @@ option(ENABLE_ASSERTS "Enable runtime assertions" ON)
 option(ENABLE_LOGGING "Enable logging" ON)
 option(ENABLE_TRACING "Enable tracing" OFF)
 option(ENABLE_SANITIZERS "Enable sanitizers" OFF)
+
+add_compile_options(-march=native)
